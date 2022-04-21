cdc1620190aac701fe0c7ac7fab2f7287adc7d76
harish <harish.kumar@digimantra,com>
2022-04-22 00:12:25 +0530

    passwd updated new

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 $con=mysqli_connect("crm-db.clp7kbjb8udx.us-east-1.rds.amazonaws.com
-", "admin", "", " phpdbcrm");
+", "admin", "pSBWXNHhlFbcbhpYHtPI", " phpdbcrm");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
