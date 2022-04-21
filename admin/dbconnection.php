e83e0c4144bf69eb992dabc61e9aa10e3607a1cb
harish <harish.kumar@digimantra,com>
2022-04-22 00:15:09 +0530

    passwd updated new

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,6 +1,5 @@
 <?php
-$con=mysqli_connect("crm-db.clp7kbjb8udx.us-east-1.rds.amazonaws.com
-", "admin", "pSBWXNHhlFbcbhpYHtPI", " phpdbcrm");
+$con=mysqli_connect("crm-db.clp7kbjb8udx.us-east-1.rds.amazonaws.com", "admin", "pSBWXNHhlFbcbhpYHtPI", "phpdbcrm");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
